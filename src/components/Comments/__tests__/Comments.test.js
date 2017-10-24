18194e38eb2030c1a98751c376ea8ee8b931a8c6
Devrsi0n <devrsi0n@gmail.com>
2017-10-24 14:35:10 +0800

    Fix `comments` unit test warning

--- src/components/Comments/__tests__/Comments.test.js
+++ src/components/Comments/__tests__/Comments.test.js
@@ -2,6 +2,9 @@ import React from "react";
 import renderer from "react-test-renderer";
 import Comments from "..";
 
+// Ignore gitment error message
+console.error = () => {};
+
 describe("<Comments />", () => {
   it("component render", () => {
     const comments = renderer.create(<Comments />).toJSON();
