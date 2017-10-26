583f5e6a82acef2870a91c7b820f3ee5b60ce675
Devrsi0n <devrsi0n@gmail.com>
2017-10-26 10:15:47 +0800

    Replace gitment with gitalk

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
-import comments from "../../utils/gitComments";
+import "gitalk/dist/gitalk.css";
+import Gitalk from "gitalk";
+// import comments from "../../utils/gitComments";
 import styles from "./comments.scss";
 import i18n from "../../utils/i18n";
 import Footer from "../Footer";
 
+const gitalk = new Gitalk({
+  clientID: "3bff581d9182d38a4598",
+  clientSecret: "f49cd76c182957bbcd2593b6d7bce5c6ae69b384",
+  repo: "React-2048-game",
+  owner: "devrsi0n",
+  admin: ["devrsi0n"],
+  // facebook-like distraction free mode
+  distractionFreeMode: false,
+  createIssueManually: true,
+  id: "https://github.com/devrsi0n/React-2048-game/issues/1",
+  labels: ["gitment", "https://devrsi0n.github.io/React-2048-game/"],
+  title: "2048",
+  body: "devrsi0n.github.io/React-2048-game"
+});
+
 export default class Comments extends Component {
   componentDidMount() {
-    comments.renderHeader(document.getElementById("gitmentHeader"));
-    comments.renderComments(document.getElementById("gitmentComments"));
-    comments.renderEditor(document.getElementById("gitmentEditor"));
+    gitalk.render("gitalk-container");
   }
 
   render() {
@@ -20,9 +35,7 @@ export default class Comments extends Component {
             className={styles.commentsTitle}
             dangerouslySetInnerHTML={{ __html: i18n.commentTitle }}
           />
-          <div id="gitmentHeader" />
-          <div id="gitmentComments" />
-          <div id="gitmentEditor" />
+          <div id="gitalk-container" />
         </div>
         <Footer
           name="devrsi0n"
